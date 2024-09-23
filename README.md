a205c14b046b06c6ac898d5af19eda74f967cf86
DavidColesDC <me@davidcoles.uk>
2024-09-23 12:47:18 +0100

    Reposit Test

--- README.md
+++ README.md
@@ -5,27 +5,31 @@ Make sure you have all of the npm packages by running
 ```
 npm install
 ```
-You can run the code from the command line in a TypeScript REPL using ts-node
-```
-ts-node
-```
 ***
 ### 1. Calculate the average rent of properties by region
+run the following in the terminal or command prompt
 ```
-getRentByRegion
+npx ts-node src/getRentByRegion.ts
 ```
+You will be prompted to enter a region, enter either of the following;
++ **England**,
++ **Wales**,
++ **Scotland** or
++ **N.Ireland**
+
+You will then receive your answer in GBP. 
 ***
 ### 2. Calculate the monthly rent, per tenant for a given property
 ```
-getRentPerTenant
+npx ts-node src/getRentPerTenant
 ```
 ***
 ### 3. Validate the postcode of all properties
 ```
-validatePostcodes
+npx ts-node src/validatePostcodes
 ```
 ***
 ### 4. Get the 'status' of a property
 ```
-getPropertyStatus
+npx ts-node src/getPropertyStatus
 ```
---
+++ src/getRentByRegion.ts
@@ -0,0 +1,49 @@
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from 'csv-parse';
+
+type PropertyDetails = {
+    id: string;
+    address: string;
+    postcode: string;
+    monthlyRentPence: number;
+    region: string;
+    capacity: number;
+    tenancyEndDate: Date;
+};
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Please enter a region? ', (region: string) => {
+    const averageRent = getRentByRegion(region);
+    console.log(averageRent);
+    readline.close();
+});
+
+export function getRentByRegion(region: string) {
+    const csvFilePath = path.resolve(__dirname, 'files/technical-challenge-properties-september-2024.csv');
+    const headers = ['id', 'address', 'postcode', 'monthlyRentPence', 'region', 'capacity', 'tenancyEndDate'];
+    const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+
+    parse(fileContent, {
+        delimiter: ',',
+        columns: headers,
+        fromLine: 2,
+        on_record: (record) => {
+            if (record.region.toUpperCase() !== region.toUpperCase()) {
+                return;
+            }
+            return record;
+        },
+    }, (error, result: PropertyDetails[]) => {
+        if (error) {
+            console.error(error);
+        }
+        let average = Math.round(result.reduce((total: number, next) => +total + +next.monthlyRentPence, 0) / result.length) / 100;
+        console.log("Average rent for", region, "is £" + average);
+    });
+    return region;
+}
