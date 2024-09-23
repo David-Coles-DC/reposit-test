43ddab0223fa78bbfd04b3973807e1210c470236
DavidColesDC <me@davidcoles.uk>
2024-09-23 18:51:02 +0100

    Reposit Test

--- src/getPropertyStatus.ts
+++ src/getPropertyStatus.ts
@@ -3,32 +3,44 @@ import * as path from "path";
 import { parse } from 'csv-parse';
 import { PropertyDetails, TenantDetails } from './types';
 
+//Creat an input for the user
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+//Creat a question for the region input
 readline.question('Please enter a property id: ', (propertyId: string) => {
+    // pass the input value to the getPropertyStatus function
     getPropertyStatus(propertyId);
     readline.close();
 });
 
+//Obtain the property status based on the user input
 export function getPropertyStatus(propertyId: string) {
+    //set the values for the CSV file
     const filePath: string = path.resolve(__dirname, 'files/technical-challenge-properties-september-2024.csv');
+    //set the headers for the CSV file
     const headers: string[] = ['id', 'address', 'postcode', 'monthlyRentPence', 'region', 'capacity', 'tenancyEndDate'];
+    //open the CSV file
     const fileContent: string = fs.readFileSync(filePath, { encoding: 'utf-8' });
 
+    //set the values for the CSV file
     const filePath2: string = path.resolve(__dirname, 'files/technical-challenge-tenants-september-2024.csv');
+    //set the headers for the CSV file
     const headers2: string[] = ['id', 'propertyId', 'name'];
+    //open the CSV file
     const fileContent2: string = fs.readFileSync(filePath2, { encoding: 'utf-8' });
 
     let propertyStatus: string = '';
 
+    //parse the CSV file and exclude the header row
     parse(fileContent, {
         delimiter: ',',
         columns: headers,
         fromLine: 2,
         on_record: (record) => {
+            //only include records where the propertyId matches the user input
             if (record.id.toUpperCase() !== propertyId.toUpperCase()) {
                 return;
             }
@@ -42,13 +54,17 @@ export function getPropertyStatus(propertyId: string) {
             console.error('Property does not exist');
             return;
         } else {
+            //get the tenancy end date for the property
             let tenancyEndDate: Date = result[0].tenancyEndDate;
+            //get the capacity value for the property
             let capacity: number = result[0].capacity;
+            //parse the CSV file and exclude the header row
             parse(fileContent2, {
                 delimiter: ',',
                 columns: headers2,
                 fromLine: 2,
                 on_record: (record) => {
+                    //only include records where the propertyId matches the user input
                     if (record.propertyId.toUpperCase() !== propertyId.toUpperCase()) {
                         return;
                     }
@@ -59,15 +75,19 @@ export function getPropertyStatus(propertyId: string) {
                     console.error(error);
                 }
                 if (result.length == 0) {
+                    //The property has no tenants
                     propertyStatus = 'PROPERTY_VACANT';
                 } else {
                     if (new Date(tenancyEndDate) > new Date(new Date())) {
                         if (capacity > result.length) {
+                            //The property is not yet at capacity and the tenancy end date has not yet expired
                             propertyStatus = 'PARTIALLY_VACANT';
                         } else {
+                            //The property is at capacity and the tenancy end date has not yet expired
                             propertyStatus = 'PROPERTY_ACTIVE';
                         }
                     } else {
+                        //The property has at least one tenant but tenancy end date has expired
                         propertyStatus = 'PROPERTY_OVERDUE';
                     }
                 }
--- src/validatePostcodes.ts
+++ src/validatePostcodes.ts
@@ -3,22 +3,29 @@ import * as path from "path";
 import { parse } from 'csv-parse';
 import { PropertyDetails } from './types';
 
+//A function to test if a postcode is valid or not, will return **true** if valid and **false** if invalid
 function validPostcode(postcode: string) {
     postcode = postcode.replace(/\s/g, "");
     let regex: RegExp = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
     return regex.test(postcode);
 }
 
+//a function that will return all propertyIds where the postcode is invalid
 export function validatePostcodes() {
+    //set the values for the CSV file
     const csvFilePath: string = path.resolve(__dirname, 'files/technical-challenge-properties-september-2024.csv');
+    //set the headers for the CSV file
     const headers: string[] = ['id', 'address', 'postcode', 'monthlyRentPence', 'region', 'capacity', 'tenancyEndDate'];
+    //open the CSV file
     const fileContent: string = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
 
+    //parse the CSV file and exclude the header row
     parse(fileContent, {
         delimiter: ',',
         columns: headers,
         fromLine: 2,
         on_record: (record) => {
+            //only include records where the postcode is invalid
             if (validPostcode(record.postcode)) {
                 return;
             }
@@ -29,8 +36,10 @@ export function validatePostcodes() {
             console.error(error);
         }
         if (result.length == 0) {
+            //all postcodes have passed as being valid
             console.log('All postcodes are valid');
         } else {
+            //display a list of propertyIds where the postcode is invalid
             console.log('The following', result.length, 'propertyIds have invalid postcodes');
             for (let i = 0; i < result.length; i++) {
                 console.log(result[i].id);
