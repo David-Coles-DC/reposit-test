a7a975a831112ad1b125d87db045b5e04c8d91dd
DavidColesDC <me@davidcoles.uk>
2024-09-25 08:34:17 +0100

    Reposit Test

--- src/openCsvFile.ts
+++ src/openCsvFile.ts
@@ -2,53 +2,24 @@ import * as fs from "fs";
 import { parse } from 'csv-parse';
 import { PropertyDetails, TenantDetails } from "./types";
 
-export async function openPropertyCsv():Promise<PropertyDetails[]> {
-    //set the headers for the CSV file*/
-    const headers: string[] = ['id', 'address', 'postcode', 'monthlyRentPence', 'region', 'capacity', 'tenancyEndDate'];
-    let records: PropertyDetails[] = [];
-
-    //process and parse the CSV file
-    const processFile = async () => {
-        const parser = fs
-            .createReadStream(`${__dirname}/files/technical-challenge-properties-september-2024.csv`)
-            .pipe(parse({
-                delimiter: ',',
-                columns: headers,
-                fromLine: 2,
-                cast: true
-            }));
-        for await (const record of parser) {
-            // Work with each record
-            records.push(record);
-        }
+async function parseCsv<T>(fileName: string, headers: string[]): Promise<T[]> {
+    const records: T[] = [];
+    const parser = fs.createReadStream(`${__dirname}/files/${fileName}`)
+        .pipe(parse({delimiter: ',', columns: headers, fromLine: 2, cast: true}));
+    for await (const record of parser) {
+        records.push(record);
     }
-
-    await processFile();
-
     return records;
 }
 
-export async function openTenantCsv():Promise<TenantDetails[]> {
-    //set the headers for the CSV file*/
-    const headers: string[] = ['id', 'propertyId', 'name'];
-    let records: TenantDetails[] = [];
+export function openPropertyCsv(): Promise<PropertyDetails[]> {
+    const filePath = 'technical-challenge-properties-september-2024.csv';
+    const headers = ['id', 'address', 'postcode', 'monthlyRentPence', 'region', 'capacity', 'tenancyEndDate'];
+    return parseCsv<PropertyDetails>(filePath, headers);
+}
 
-    //process and parse the CSV file
-    const processFile = async () => {
-        const parser = fs
-            .createReadStream(`${__dirname}/files/technical-challenge-tenants-september-2024.csv`)
-            .pipe(parse({
-                delimiter: ',',
-                columns: headers,
-                fromLine: 2
-            }));
-        for await (const record of parser) {
-            // Work with each record
-            records.push(record);
-        }
-    }
-
-    await processFile();
-
-    return records;
+export function openTenantCsv(): Promise<TenantDetails[]> {
+    const filePath = 'technical-challenge-tenants-september-2024.csv';
+    const headers = ['id', 'propertyId', 'name'];
+    return parseCsv<TenantDetails>(filePath, headers);
 }
