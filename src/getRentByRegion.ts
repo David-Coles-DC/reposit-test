f84e6a12952ffd9c2328db7641ef898637de061d
DavidColesDC <me@davidcoles.uk>
2024-09-25 09:05:59 +0100

    Reposit Test

--- src/getRentByRegion.ts
+++ src/getRentByRegion.ts
@@ -1,5 +1,4 @@
-import { PropertyDetails } from './types';
-import { openPropertyCsv } from './openCsvFile';
+import { getRentByRegion } from './utils/getRentByRegion';
 import * as readline from "readline";
 
 //Create an input for the user
@@ -9,31 +8,9 @@ const rl = readline.createInterface({
 });
 
 //Create a question for the region input
-rl.question('Please enter a region: ', (region: string) => {
+rl.question('Please enter a region: ', async (region: string) => {
     // pass the input value to the getRentByRegion function
-    getRentByRegion(region).then(r => {
-        rl.close();
-    });
+    const average = await getRentByRegion(region);
+    console.log(`Average rent for ${region} is £${average.toFixed(2)}`);
+    rl.close();
 });
-
-//Obtain the average rent by region based on the user input
-export async function getRentByRegion(region: string) {
-    let matchedProperties: PropertyDetails[] = [];
-    let average: number = 0;
-
-    await openPropertyCsv()
-        .then((data) => {
-            //filter the property data by the region that the user entered
-            matchedProperties = data.filter((record: PropertyDetails) => record.region.toUpperCase() == region.toUpperCase());
-        });
-
-    if (matchedProperties.length == 0) {
-        //if no results match the user input
-        console.error('Region does not exist');
-    } else {
-        //calculate the average rent and show it to the user
-        average = Math.round(matchedProperties.reduce((total: number, next) => +total + +next.monthlyRentPence, 0) / matchedProperties.length) / 100;
-        console.log(`Average rent for ${region} is £${average}`);
-    }
-    return average;
-}
--- src/utils/getRentByRegion.ts
+++ src/utils/getRentByRegion.ts
@@ -1,5 +1,5 @@
-import { PropertyDetails } from './types';
-import { openPropertyCsv } from './openCsvFile';
+import { PropertyDetails } from '../types';
+import { openPropertyCsv } from '../openCsvFile';
 
 //Obtain the average rent by region based on the user input
 export async function getRentByRegion(region: string) {
@@ -7,18 +7,16 @@ export async function getRentByRegion(region: string) {
     let average: number = 0;
 
     await openPropertyCsv()
-        .then((data) => {
+        .then((data: PropertyDetails[]) => {
             //filter the property data by the region that the user entered
             matchedProperties = data.filter((record: PropertyDetails) => record.region.toUpperCase() == region.toUpperCase());
         });
 
     if (matchedProperties.length == 0) {
         //if no results match the user input
-        console.error('Region does not exist');
+        throw new Error(`No properties found for ${region}`);
     } else {
-        //calculate the average rent and show it to the user
-        average = Math.round(matchedProperties.reduce((total: number, next) => +total + +next.monthlyRentPence, 0) / matchedProperties.length) / 100;
-        console.log(`Average rent for ${region} is £${average}`);
+        //calculate the average rent
+        return matchedProperties.reduce((total, next) => total + next.monthlyRentPence, 0) / matchedProperties.length / 100;
     }
-    return average;
 }
